4fe2975bc7d2ab8c1712a54570adbc39d3502052
yara-stef <ymhladyak@gmail.com>
2022-12-12 09:39:37 +0000

    added eventListener to size btn

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-'use strict';
+// 'use strict';
 
-const size = document.querySelectorAll('.btn-size');
+const btnSize = document.querySelector('.btn-size');
+let index = 0;
+const backgroundColors = ['black', 'rgb(231, 234, 238)'];
+const colors = ['white', 'black'];
 
-const switchButtonColor = function () {
-    size.classList.remove('.inactive');
-    size.classList.add('.active');
-}
-console.log(switchButtonColor());
+btnSize.addEventListener('click', function () {
+    btnSize.style.backgroundColor = backgroundColors[index];
+    btnSize.style.color = colors[index];
+    
+    index = index >= backgroundColors.length - 1 ? 0 : index + 1;
+});
+    
 
-
-size.addEventListener('click', switchButtonColor());
