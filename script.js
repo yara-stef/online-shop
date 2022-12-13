05ac157ad300de7048cab89e731d7ea3db6a9749
yara-stef <ymhladyak@gmail.com>
2022-12-13 13:19:27 +0000

    fixed problem with addToCart btns

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-// 'use strict';
+'use strict';
 
-const btnSizes = document.querySelectorAll('.btn-size');
-debugger;
+const btnsSize = document.querySelectorAll('.btn-size');
 let index = 0;
 const backgroundColors = ['black', 'rgb(231, 234, 238)'];
 const colors = ['white', 'black'];
-const btnAddToCart = document.querySelector('.btn-item');
+const btnsAddToCart = document.querySelectorAll('.btn-item');
 const addToCartBackground = ['yellow', 'black'];
 const addToCartColors = ['black', 'white'];
 let cart = [];
 
-Array.from(btnSizes).map(btnSize => {
+//btnsSize - це node list, а btnSize - це його елемент. Перетворюємо node list в масив за допомогою Array.from(), 
+// і далі до нього застосовуємо метод масивів map(), який виконує ітерацію кожного елемента.
+
+Array.from(btnsSize).map(btnSize => {
     btnSize.addEventListener('click', function () {
     btnSize.style.backgroundColor = backgroundColors[index];
     btnSize.style.color = colors[index];
     
     index = index >= backgroundColors.length - 1 ? 0 : index + 1;
+    
 });
 })
 
 
 
-
-btnAddToCart.addEventListener('click', function () {
+Array.from(btnsAddToCart).map(btnAddToCart => {
+    btnAddToCart.addEventListener('click', function () {
     btnAddToCart.style.backgroundColor = addToCartBackground[index];
     btnAddToCart.style.color = addToCartColors[index];
 
     index = index >= addToCartBackground.length - 1 ? 0 : index + 1;
 });
+})
+
     
 
