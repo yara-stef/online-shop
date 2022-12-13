1fd71b92a6dd5e133f08a8b8d3d3af5687aa342d
yara-stef <ymhladyak@gmail.com>
2022-12-13 09:04:46 +0000

    added eventListener to add ToCart button

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ const btnSize = document.querySelector('.btn-size');
 let index = 0;
 const backgroundColors = ['black', 'rgb(231, 234, 238)'];
 const colors = ['white', 'black'];
+const btnAddToCart = document.querySelector('.btn-item');
+const addToCartBackground = ['yellow', 'black'];
+const addToCartColors = ['black', 'white'];
 
 btnSize.addEventListener('click', function () {
     btnSize.style.backgroundColor = backgroundColors[index];
     btnSize.style.color = colors[index];
     
     index = index >= backgroundColors.length - 1 ? 0 : index + 1;
+});
+
+btnAddToCart.addEventListener('click', function () {
+    btnAddToCart.style.backgroundColor = addToCartBackground[index];
+    btnAddToCart.style.color = addToCartColors[index];
+
+    index = index >= addToCartBackground.length - 1 ? 0 : index + 1;
 });
     
 
