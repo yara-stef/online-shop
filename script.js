cdcad09ab5f5f6b1e382ab3ce5efd21673239437
yara-stef <ymhladyak@gmail.com>
2022-12-20 16:27:29 +0000

    working on shopping cart

--- script.js
+++ script.js
@@ -216,3 +216,98 @@ function sizeXXL() {
     } 
     itemsFound.textContent = `6 Product(s) found`;
 }
+
+// Cart
+
+const shoppingCart = function () {
+    cart = [];
+
+    // Constructor
+    function Item(name, price, count) {
+        this.name = name;
+        this.price = price;
+        this.count = count
+    }
+
+    // Save cart
+    function saveCart() {
+        sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+
+    // Load cart
+    function loadCart() {
+        cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+    }
+    if (sessionStorage.getItem != null) {
+        loadCart();
+    }
+
+    const obj = {};
+
+    // Add to cart
+    obj.addItemToCart = function (name, price, count) {
+        for (const item in cart) {
+            if (cart[item].name === name) {
+                cart[item].count++;
+                saveCart();
+                return;
+            }
+        }
+        const item = new Item(name, price, count);
+        cart.push(item);
+        saveCart();
+    }
+
+    // Set count for item
+    obj.setCountForItem = function (name, count) {
+        for (const i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+    };
+
+    // Remove item fron cart
+    obj.removeItemFromCart = function (name) {
+        for (const item in cart) {
+            if (cart[item].name === name) {
+                cart[item].count--;
+                if (cart[item].name === 0) {
+                    cart.splice(item, 1);
+                }
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // Remove all items from cart
+    obj.removeItemsFromCartAll = function (name) {
+        for (const item in cart) {
+            if (cart[item].name === name) {
+                cart.splice(item, 1);
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // Clear cart
+    obj.clearCart = function () {
+        cart =[];
+        saveCart();
+    }
+
+    // Count cart
+    obj.totalCount = function () {
+        const totalCount = 0;
+        for (const item in cart) {
+            totalCount += cart[item].count;
+        }
+        return totalCount;
+    }
+
+    // Total cart
+    
+}
